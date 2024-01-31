5cb2814f61dd2c5c16dbbade92bac4197075f6da
noushad <120626388+noushad257@users.noreply.github.com>
2024-01-31 14:57:58 +0600

    For-each

---
+++ Loop_lec1.java
@@ -0,0 +1,10 @@
+public class Loop_lec1  {
+    public static void main(String[] args) {
+        String[]animal={"cat","dog","bird"};
+        for(String i:animal){
+            System.out.println(i);
+        }
+    }
+}
+
+
